9912b9129f8afecd19266c4fff62c67d34f28942
KitKatNeko <7532730+kitkatneko@users.noreply.github.com>
2020-07-28 10:04:33 -0400

    mo' comments

--- update-sigsci_agent.ps1
+++ update-sigsci_agent.ps1
@@ -1,3 +1,4 @@
+# Script to "auto" update the Signal Sciences Agent
 Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-agent/sigsci-agent_latest.msi" -OutFile C:\Apps\sigsci\agent\sigsci-agent_latest.msi
 C:\Apps\sigsci\agent\sigsci-agent_latest.msi /quiet
 sleep 60
