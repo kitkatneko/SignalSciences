20c889d272d2848ee458205b4b940603f9d4b49e
KitKatNeko <7532730+kitkatneko@users.noreply.github.com>
2020-07-28 10:02:09 -0400

    Create update-sigsci_agent.ps1

---
+++ update-sigsci_agent.ps1
@@ -0,0 +1,7 @@
+Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-agent/sigsci-agent_latest.msi" -OutFile C:\Apps\sigsci\agent\sigsci-agent_latest.msi
+C:\Apps\sigsci\agent\sigsci-agent_latest.msi /quiet
+sleep 60
+stop-service sigsci-agent
+start-service sigsci-agent
+sleep 30
+iisreset
