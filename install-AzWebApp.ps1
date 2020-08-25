b6de366db2b3e86a35e528996732cb4449ea8dec
KitKatNeko <7532730+kitkatneko@users.noreply.github.com>
2020-08-25 17:03:35 -0400

    Update install-AzWebApp.ps1

--- install-AzWebApp.ps1
+++ install-AzWebApp.ps1
@@ -3,7 +3,7 @@
 #https://docs.signalsciences.net/install-guides/paas/azure-app-service/
 
 #Vars
-$TargetRG = "dcac-sitecore-rg"
+$TargetRG = "YourTarget"
 $TargetWebApp = "YourWebApp"
 $ACCESSKEYID = "yourkey"
 $SECRETACCESSKEY = "yourkey"
@@ -12,5 +12,7 @@ $SECRETACCESSKEY = "yourkey"
 Get-AzWebApp -ErrorAction Ignore -PipelineVariable webapp -ResourceGroupName $TargetRG | where {$webapp.HttpsOnly -ne $true} | foreach {
   # Set the settings
   $webapp | set-azwebapp #--settings  SIGSCI_ACCESSKEYID=$ACCESSKEYID
+  # install the extension
+  # New-AzureRmResource -ResourceType "Microsoft.Web/sites/siteextensions" -ResourceGroupName MyResourceGroup -Name "MyWebApp/SiteExtensionName" -ApiVersion "2018-02-01" -Force
 }
 
