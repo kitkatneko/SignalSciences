5af77b7c9f01e953e22eb52e32f88f748fa1f9b0
KitKatNeko <7532730+kitkatneko@users.noreply.github.com>
2020-11-26 10:28:29 -0500

    added a delay

--- update-IIS-agent-module-norestart.ps1
+++ update-IIS-agent-module-norestart.ps1
@@ -5,9 +5,12 @@ mkdir c:\apps\sigsci\module
 
 # Script to "auto" update the Signal Sciences Agent
 Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-agent/sigsci-agent_latest.msi" -OutFile C:\Apps\sigsci\agent\sigsci-agent_latest.msi
-msiexec /i C:\Apps\sigsci\agent\sigsci-agent_latest.msi
+msiexec /i C:\Apps\sigsci\agent\sigsci-agent_latest.msi /qn
+
+# Wait for it...
+sleep 60
 
 # Script to "auto" update the Signal Sciences IIS Module
 Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-module-iis/sigsci-module-iis_latest.msi" -OutFile C:\Apps\sigsci\module\sigsci-module-iis_latest.msi 
-msiexec /i C:\Apps\sigsci\module\sigsci-module-iis_latest.msi 
+msiexec /i C:\Apps\sigsci\module\sigsci-module-iis_latest.msi /qn
 
