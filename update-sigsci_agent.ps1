cd321c70e8b9db0d8bb61fa460a383db204d9aaa
KitKatNeko <7532730+kitkatneko@users.noreply.github.com>
2020-11-26 09:58:36 -0500

    added tls1.2 as per sigsci website for sources.

--- update-sigsci_agent.ps1
+++ update-sigsci_agent.ps1
@@ -1,4 +1,5 @@
 # Script to "auto" update the Signal Sciences Agent
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-agent/sigsci-agent_latest.msi" -OutFile C:\Apps\sigsci\agent\sigsci-agent_latest.msi
 C:\Apps\sigsci\agent\sigsci-agent_latest.msi /quiet
 sleep 60
