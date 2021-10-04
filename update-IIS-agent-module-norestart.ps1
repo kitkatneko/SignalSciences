88cdc80dd9d9b17b5407391bb1a60b70aa0546aa
KitKatNeko <7532730+kitkatneko@users.noreply.github.com>
2021-10-04 15:52:46 -0400

    powershell edition

--- update-IIS-agent-module-norestart.ps1
+++ update-IIS-agent-module-norestart.ps1
@@ -6,13 +6,12 @@ if (test-path $AgentPath) {} else {New-Item -Path $AgentPath -ItemType Directory
 if (test-path $ModulePath) {} else {New-Item -Path $ModulePath -ItemType Directory}
 
 # Script to "auto" update the Signal Sciences Agent
-Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-agent/sigsci-agent_latest.msi" -OutFile C:\Apps\sigsci\agent\sigsci-agent_latest.msi
-msiexec /i C:\Apps\sigsci\agent\sigsci-agent_latest.msi /qn
+Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-agent/sigsci-agent_latest.msi" -OutFile $AgentPath\sigsci-agent_latest.msi
+msiexec /i $AgentPath\sigsci-agent_latest.msi /qn
 
 # Wait for it...
-sleep 60
+start-sleep -s 60
 
 # Script to "auto" update the Signal Sciences IIS Module
-Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-module-iis/sigsci-module-iis_latest.msi" -OutFile C:\Apps\sigsci\module\sigsci-module-iis_latest.msi 
-msiexec /i C:\Apps\sigsci\module\sigsci-module-iis_latest.msi /qn
-
+Invoke-WebRequest -uri "https://dl.signalsciences.net/sigsci-module-iis/sigsci-module-iis_latest.msi" -OutFile $ModulePath\sigsci-module-iis_latest.msi 
+msiexec /i $ModulePath\sigsci-module-iis_latest.msi /qn
